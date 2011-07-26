40c3a2bf92c54df8336ca2b54065f1635071416d
moops <work@raceweb.ca>
2011-07-26 11:18:27 -0700

    find a wava for a non-standard distance

--- app/models/wava.rb
+++ app/models/wava.rb
@@ -1,14 +1,32 @@
 class Wava < ActiveRecord::Base
 
   def self.find_for(age, gender, distance)
-    max_dist = distance + 0.2
-    min_dist = distance - 0.2
-    Wava.where('age = :age and gender = :gender and distance > :min_dist and distance < :max_dist', {:age => age, :gender => gender, :min_dist => min_dist, :max_dist => max_dist}).first
+    age_and_gender = 'age = :age and gender = :gender'
+    w = Wava.where(age_and_gender << ' and distance > :min and distance < :max', {:age => age, :gender => gender, :min => distance - 0.2, :max => distance + 0.2}).first
+    unless w
+      # didn't find one. distance probably doesn't match
+      upper = find_for_next_distance(age, gender, distance)
+      lower = find_for_previous_distance(age, gender, distance)
+      if (upper and lower)
+        # found both - use weighted average
+        factor = lower.factor + ((distance - lower.distance) / (upper.distance - lower.distance) * (upper.factor - lower.factor))
+      end
+      w = Wava.new({:age => age, :gender => gender, :distance => distance, :factor => factor})
+    end
+    w
+  end
+  
+  def self.find_for_next_distance(age, gender, distance)
+    age_and_gender = 'age = :age and gender = :gender'
+    Wava.where(age_and_gender << ' and distance > :dist', {:age => age, :gender => gender, :dist => distance}).order('distance asc').first
+  end
+  
+  def self.find_for_previous_distance(age, gender, distance)
+      age_and_gender = 'age = :age and gender = :gender'
+      Wava.where(age_and_gender << ' and distance < :dist', {:age => age, :gender => gender, :dist => distance}).order('distance asc').last
   end
   
   def self.list_for(distance)
-    max_dist = distance + 0.2
-    min_dist = distance - 0.2
-    Wava.where('distance > :min_dist and distance < :max_dist', {:min_dist => min_dist, :max_dist => max_dist}).all
+    Wava.where('distance > :min and distance < :max', {:min => distance - 0.2, :max => distance + 0.2}).all
   end
 end
